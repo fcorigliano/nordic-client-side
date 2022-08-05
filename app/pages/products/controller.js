be99a09a446c7660b0b7da2193929d4f5a9c063e
Facundo Corigliano <facundo.corigliano@mercadolibre.com>
2022-08-04 23:28:42 -0300

    i18n e Image

--- app/pages/products/controller.js
+++ app/pages/products/controller.js
@@ -1,8 +1,29 @@
-const React = require('react');
-const View = require('./view');
- 
+const React = require("react");
+const View = require("./view");
+const ImageProvider = require("nordic/image/provider");
+const config = require("nordic/config");
+const I18nProvider = require("nordic/i18n/I18nProvider");
+
+const imagesPrefix = config.assets.prefix;
+
 exports.render = function render(req, res) {
-  const Products = (props) => <View {...props} />;
- 
-  res.render(Products);
+  const Products = props => (
+    <I18nProvider i18n={req.i18n}>
+      <ImageProvider prefix={imagesPrefix}>
+        <View {...props} />
+      </ImageProvider>
+    </I18nProvider>
+  )
+
+  res.render(Products, {
+    translations: req.translations,
+    imagesPrefix,
+  }, {
+    layoutOptions: {
+      staticMarkup: false,
+    },
+    navigationOptions: {
+      type: 'full',
+    },
+    });
 };
--- app/pages/products/view.js
+++ app/pages/products/view.js
@@ -1,30 +1,69 @@
-const React = require('react');
-const Script = require('nordic/script');
+const React = require("react");
+const Script = require("nordic/script");
+const Head = require('nordic/head');
+const Image = require("nordic/image");
+const { injectI18n } = require("nordic/i18n");
+const serialize = require("serialize-javascript");
 
-const restclient = require('nordic/restclient')({
-    timeout: 5000,
-    baseURL: '/api'
+const restclient = require("nordic/restclient")({
+  timeout: 5000,
+  baseURL: "/api",
 });
 
-const { useState } = React;
-const head = require('nordic/head');
+const { useState, useEffect } = React;
 
-function View(props){
-    const [ data, setData ] = useState([])
-    React.useEffect(() => {
-        restclient.get('/get-products')
-            .then(res => setData(res.data))
-    }, []);
+function View(props) {
+  const { imagesPrefix, translations, i18n } = props;
 
-    return(<>
-        <Script src='vendor.js' /> 
-        <Script src='products.js' />
+  const preloadedState = { imagesPrefix, translations, i18n };
 
-        <ul>
-            {data.length > 0 ? data.map(p => <li>{p.title}</li>) : <li>No hay nada</li>}
-        </ul>
-    </>);
+  const [products, setProducts] = useState([]);
 
+  useEffect(() => {
+    restclient
+      .get("/get-products", {
+        params: {
+          name: "tablet",
+        },
+      })
+      .then(res => setProducts(res.data));
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>
+          Products Page
+        </title>
+      </Head>
+      <Script>
+        {`
+          window.__PRELOADED_STATE__ = ${serialize(preloadedState, { isJSON: true })};
+          console.log('Products page is loaded!');
+        `}
+      </Script>
+      <Script src="vendor.js" />
+      <Script src="products.js" />
+
+      <ul>
+        {products.length > 0
+          ? products.map(item => (
+              <li key={item.id}>
+                <h5>{i18n.gettext(item.title)}</h5>
+                <a href={item.permalink}>
+                  <Image
+                    src={item.thumbnail}
+                    alt={i18n.gettext(item.title)}
+                    lazyload="off"
+                  />
+                </a>
+                <p>{item.price}</p>
+              </li>
+            ))
+          : ""}
+      </ul>
+    </>
+  );
 }
 
-module.exports = View;
+module.exports = injectI18n(View);
